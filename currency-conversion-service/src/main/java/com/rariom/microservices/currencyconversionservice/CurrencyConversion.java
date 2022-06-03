9cf3d9f287e08d3512be20b470393c0a78dcd158
Marianne Rario <mariannecastillorario27@gmail.com>
2022-06-04 01:57:50 +0800

    Invoked Currency Exchange from Currency Conversion Microservice

--- currency-conversion-service/src/main/java/com/rariom/microservices/currencyconversionservice/CurrencyConversion.java
+++ currency-conversion-service/src/main/java/com/rariom/microservices/currencyconversionservice/CurrencyConversion.java
@@ -6,8 +6,8 @@ public class CurrencyConversion {
     private Long id;
     private String from;
     private String to;
-    private BigDecimal conversionMultiple;
     private BigDecimal quantity;
+    private BigDecimal conversionMultiple;
     private BigDecimal totalCalculatedAmount;
     private String environment;
 
--- currency-conversion-service/src/main/java/com/rariom/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/rariom/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.rariom.microservices.currencyconversionservice;
 
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/currency-conversion")
@@ -20,9 +23,25 @@ public class CurrencyConversionController {
             @PathVariable BigDecimal quantity){
 
         // call the currency exchange microservice using RestTemplate
-        new RestTemplate()
-                .getForEntity()
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate()
+                .getForEntity(
+                        "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyConversion.class,
+                        uriVariables);
+        System.out.println("mima: " + responseEntity);
+        // get the value of response entity
+        CurrencyConversion currencyConversion = responseEntity.getBody();
         return new CurrencyConversion(
-                1001L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+                currencyConversion.getId(),
+                from,
+                to,
+                quantity,
+                currencyConversion.getConversionMultiple(),
+                quantity.multiply(currencyConversion.getConversionMultiple()),
+                currencyConversion.getEnvironment()
+                );
     }
 }
