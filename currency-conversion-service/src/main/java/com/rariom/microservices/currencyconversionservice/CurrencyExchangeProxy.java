eb3256b8765e8c2c7364468218d9b18cdfa712e6
Marianne Rario <mariannecastillorario27@gmail.com>
2022-06-06 20:52:52 +0800

    load balance microservices using eureka, feign, and spring cloud loadbalancer

--- currency-conversion-service/src/main/java/com/rariom/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/rariom/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -10,7 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 // Currency Conversion Proxy
 // the name should be the same as the name you provide in the application.properties of the
 // microservice you want to connect "currency-exchange"
-@FeignClient(name = "currency-exchange", url = "localhost:8000")
+//@FeignClient(name = "currency-exchange", url = "localhost:8000")
+@FeignClient(name = "currency-exchange") // make the feign client talk to the eureka and pick up the instance of currency exchange and do load balancing between them
 public interface CurrencyExchangeProxy {
 
     // from currency exchange controller
